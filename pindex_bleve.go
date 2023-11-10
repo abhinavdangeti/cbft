b8bfc448346e5dca601c99b2b26161f04a606995
Abhinav Dangeti <abhinav@couchbase.com>
2023-11-10 10:30:03 -0700

    MB-59592: Backport support for VS
    Includes:
    
    * 14b59f58 Abhinav Dangeti | Move up bleve@unstable
      + bleve:
        * fcd85c86 Rahul Rampure | added dot_product tests (#1922)
        * 486cb007 Abhi Dangeti | Close the opened vector index sooner than later (at end of fn) (#1925)
        * 81e98987 Abhi Dangeti | Optimize multi kNN search over the same field (#1924)
        * 76635b5e Rahul Rampure | change score breakdown from array to map (#1921)
      + zapx:
        * 89e8af4 Abhi Dangeti | Improve search path for multi kNN and more necessarily for single kNN (#190)
        * 90bb10c Aditi Ahuja | Separate functions to read and search vector indexes. (#184)
        * 9349952 Thejas-bhat | Resetting the fields of opaque for resusability (#189)
        * acaf783 Thejas-bhat | bug fix: fixing the logic of tracking vector ids during a merge (#187)
        * 7404758 Thejas-bhat | fixing IO stats computation (#188)
    * 5ae2216f Abhinav Dangeti | Upgrade bleve@unstable and zapx/v16@master
      + bleve:
        * fd6589c4 Rahul Rampure | integrate knn operator (#1920)
        * 835f042e Aditi Ahuja | Adding KNN scorer explanation (#1899)
        * 6291df28 Rahul Rampure | MB-59766: Fix total calculation (#1918)
        * 9bf68eeb Abhi Dangeti | Move up zap@v16 (#1919)
        * eb56f035 Abhi Dangeti | Cleanup TestKNNOperator, collapse unit test into knn_test.go (#1917)
        * b5c245b3 Aditi Ahuja | Add KNN Operator (#1908)
        * f919433e Rahul Rampure | MB-59102: Merging KNN Results (#1910)
        * 44f1dc20 Abhi Dangeti | Upgrade zapx@v16 (#1916)
        * 645d0e31 Abhi Dangeti | Merge remote-tracking branch 'origin/master' into unstable (#1913)
        * fa9254ea Likith B | MB-59673: Added validation to KNN queries (#1912)
        * fdb65883 Abhi Dangeti | MB-59692: Upgrade to bleve_index_api@v1.1.3 (#1911)
      + zapx:
        * 322e3d7 Thejas-bhat | choosing the index types more responsibly (#186)
        * dd26ea1 Thejas-bhat | command line tool support for vector search (#175)
        * d292ef9 Thejas-bhat | replacing the flat index with the IVF family index. (#180)
        * f5e149a Thejas-bhat | bug fix: accounting the vecIDs approriately while merging (#179)
        * 0296d71 Thejas-bhat | optimization: avoiding storing of bitmaps when vector is in a single document (#181)
        * 6b9b047 Abhi Dangeti | MB-59692: Respect choice of similarity metric, bleve_index_api@v1.1.3 (#182)
        * e1fe4fb Abhi Dangeti | MB-59569: Upgrade to blevesearch/go-faiss@v1.0.3 (#183)
    * 7c9d01f7 Aditi Ahuja | MB-59573 - Support KNN Operator
    * 48e80a39 Abhinav Dangeti | Upgrade bleve/v2@unstable
    * a1a14e45 Abhinav Dangeti | MB-58902: CB_BUILD_FAISS -> CB_USE_FAISS
    * 1fb9f97b Abhinav Dangeti | MB-59507: Upgrade bleve, zapx, go-faiss versions
    * afa649fa Chris Hillery | MB-58902: Add faiss_c dependency
    * fe7b7ca7 Abhinav Dangeti | MB-58938: Really bring in bleve with zap/v16
    * 0cea75df Abhinav Dangeti | Move back preferred zap version to 15
    * 08f2b839 Abhinav Dangeti | Redirect to bleve@master
    * 303de3f3 Abhinav Dangeti | MB-58938: Bring in bleve with zap/v16
    * 2694d0dc Abhinav Dangeti | MB-58938: Support vector data type in UI + Search requests
    
    Change-Id: Ia54c4fa28fc0a6bdc7148a89deaed870e287b2b2

--- pindex_bleve.go
+++ pindex_bleve.go
@@ -87,7 +87,7 @@ const BleveDefaultZapVersion = int(11)
 // BlevePreferredZapVersion is the recommended zap version for newer indexes.
 // This version needs to be bumped to reflect the latest recommended zap
 // version in any given release.
-var BlevePreferredZapVersion = int(15)
+var BlevePreferredZapVersion = int(16)
 
 var defaultLimitingMinTime = 500
 var defaultLimitingMaxTime = 120000
@@ -230,6 +230,8 @@ type SearchRequest struct {
 	Limit            *int                    `json:"limit,omitempty"`
 	Offset           *int                    `json:"offset,omitempty"`
 	Collections      []string                `json:"collections,omitempty"`
+	KNN              json.RawMessage         `json:"knn,omitempty"`
+	KNNOperator      json.RawMessage         `json:"knn_operator,omitempty"`
 }
 
 func (sr *SearchRequest) ConvertToBleveSearchRequest() (*bleve.SearchRequest, error) {
@@ -283,6 +285,10 @@ func (sr *SearchRequest) ConvertToBleveSearchRequest() (*bleve.SearchRequest, er
 		if err != nil {
 			return nil, err
 		}
+	}
+
+	if r, err = interpretKNNForRequest(sr.KNN, sr.KNNOperator, r); err != nil {
+		return nil, err
 	}
 
 	return r, r.Validate()
@@ -582,6 +588,15 @@ func PrepareIndexDef(mgr *cbgt.Manager, indexDef *cbgt.IndexDef) (
 				}
 			}
 		}
+
+		if !isClusterCompatibleFor(FeatureVectorSupportVersion) {
+			// Vector indexing & search is NOT supported on this cluster
+			// (lower version or mixed lower version)
+			if vectorFieldsExistWithinIndexMapping(bp.Mapping) {
+				return nil, fmt.Errorf("PrepareIndex, err: vector typed fields " +
+					"not supported in mixed version cluster")
+			}
+		}
 	}
 
 	segmentVersionSupported := cbgt.IsFeatureSupportedByCluster(
@@ -629,6 +644,49 @@ func PrepareIndexDef(mgr *cbgt.Manager, indexDef *cbgt.IndexDef) (
 		return rv, cbgt.NewInternalServerError("%v", err)
 	}
 	return rv, nil
+}
+
+// Utility function check if a "vector" typed field is present within
+// the index mapping
+func vectorFieldsExistWithinIndexMapping(m mapping.IndexMapping) bool {
+	im, ok := m.(*mapping.IndexMappingImpl)
+	if !ok {
+		// cannot interpret index mapping
+		return false
+	}
+
+	var vectorFieldExistsWithinDocMapping func(*mapping.DocumentMapping) bool
+	vectorFieldExistsWithinDocMapping = func(d *mapping.DocumentMapping) bool {
+		if d != nil && d.Enabled {
+			for _, v := range d.Properties {
+				if vectorFieldExistsWithinDocMapping(v) {
+					return true
+				}
+			}
+
+			for _, field := range d.Fields {
+				if field.Type == "vector" {
+					return true
+				}
+			}
+		}
+
+		return false
+	}
+
+	// Check DefaultMapping
+	if vectorFieldExistsWithinDocMapping(im.DefaultMapping) {
+		return true
+	}
+
+	// Iterate over TypeMapping(s)
+	for _, d := range im.TypeMapping {
+		if vectorFieldExistsWithinDocMapping(d) {
+			return true
+		}
+	}
+
+	return false
 }
 
 func ValidateBleve(indexType, indexName, indexParams string) error {
@@ -1035,6 +1093,7 @@ func bleveRuntimeConfigMap(bleveParams *BleveParams) (map[string]interface{},
 		"rollbackSamplingInterval": "10m",
 		"forceSegmentType":         "zap",
 		"bolt_timeout":             "30s",
+		// enable_concurrency will be added here.
 	}
 	for k, v := range bleveParams.Store {
 		if k == "segmentVersion" {
